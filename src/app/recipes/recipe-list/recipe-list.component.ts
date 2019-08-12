a22d13cf412a944f1b4c9b195f737f58253a95f1
Maria A Perez <mperez@wbinnova.com>
2019-08-12 15:06:31 -0400

    First Commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [
+    new Recipe('A test','This is a test', 'https://www.maxpixel.net/photo-3836174')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
